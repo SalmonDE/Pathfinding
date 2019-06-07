734a3b422e8c811289328b2f86d6a628b9245783
SalmonDE <salmonde@salmonde.de>
2019-06-08 00:18:00 +0200

    Micro-optimisation in XYZ and XZ selectors

--- src/salmonde/pathfinding/astar/selector/NeighbourSelectorXYZ.php
+++ src/salmonde/pathfinding/astar/selector/NeighbourSelectorXYZ.php
@@ -4,10 +4,18 @@ declare(strict_types = 1);
 namespace salmonde\pathfinding\astar\selector;
 
 use pocketmine\block\Block;
+use pocketmine\math\Facing;
 
 class NeighbourSelectorXYZ implements NeighbourSelector {
 
 	public function getNeighbours(Block $block): array{
-		return $block->getAllSides();
+		return [
+			$block->getSide(Facing::UP),
+			$block->getSide(Facing::DOWN),
+			$block->getSide(Facing::NORTH),
+			$block->getSide(Facing::SOUTH),
+			$block->getSide(Facing::WEST),
+			$block->getSide(Facing::EAST)
+		];
 	}
 }
--- src/salmonde/pathfinding/astar/selector/NeighbourSelectorXZ.php
+++ src/salmonde/pathfinding/astar/selector/NeighbourSelectorXZ.php
@@ -4,10 +4,16 @@ declare(strict_types = 1);
 namespace salmonde\pathfinding\astar\selector;
 
 use pocketmine\block\Block;
+use pocketmine\math\Facing;
 
 class NeighbourSelectorXZ implements NeighbourSelector {
 
 	public function getNeighbours(Block $block): array{
-		return $block->getHorizontalSides();
+		return [
+			$block->getSide(Facing::NORTH),
+			$block->getSide(Facing::SOUTH),
+			$block->getSide(Facing::WEST),
+			$block->getSide(Facing::EAST)
+		];
 	}
 }
